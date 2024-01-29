0a5651c79c011adca5d89eee3f1a23b780bfb3e4
MaithiliBhurke <maithilibhurke10.model@gmail.com>
2024-01-29 22:57:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+
+
+const bar = document.querySelector('#bar');
+const cross = document.querySelector('#cross');
+const nav = document.querySelector('.nav');
+const arrow = document.querySelectorAll('.arrow');
+
+
+bar.addEventListener('click', () => {
+    bar.style.display = 'none';
+    cross.style.display = 'block';
+    nav.style.left = '20%';
+})
+
+cross.addEventListener('click', () => {
+    cross.style.display = 'none';
+    bar.style.display = 'block';
+    nav.style.left = '100%';
+
+})
+
+arrow.forEach((e) => {
+
+    e.addEventListener('click', () => {
+        var footerlist = e.parentElement.nextElementSibling;
+
+
+        if(e.classList.contains('temp')){
+            e.classList.remove('temp');
+            footerlist.style.display='none';
+            e.style.transform='rotate(0deg)';
+        }else{
+            e.classList.add('temp');
+            footerlist.style.display='block';
+            e.style.transform='rotate(180deg)';
+        }
+
+
+    });
+
+})
+
+
+// DOM manipulation
+
+const menuNav=document.querySelector('.menu-nav');
+
+window.addEventListener('scroll', ()=>{
+    if(window.pageYOffset>menuNav.offsetTop)  {
+        menuNav.classList.add('sticky');
+    }else{
+        menuNav.classList.remove('sticky');
+    }
+})
+
+
+
+
